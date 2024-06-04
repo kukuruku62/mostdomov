ca30e1c8fcd94573ed788396e37c021b97db30b8
Aleksei Falchuk <sddw28@gmail.com>
2024-06-04 10:55:52 +0200

    feat: add gallery component

---
+++ front/src/components/Gallery/Gallery.tsx
@@ -0,0 +1,44 @@
+import { useId } from "react";
+import styles from "./Gallery.module.scss";
+
+export const Gallery = () => {
+  const data = [
+    {
+      id: useId(),
+      url: "../../../src/assets/img/1.webp",
+      name: "Mudr. Zuzana Hajster Vozárová",
+      profession: "Odborná garantka, lekárka",
+      email: "hajstervozarova@mostdomov.com",
+    },
+    {
+      id: useId(),
+      url: "../../../src/assets/img/2.webp",
+      name: "Mgr. Simona Hornačková, dipl.s.",
+      profession: "Zdravotná sestra",
+      email: "simona.hornackova@mostdomov.com",
+    },
+
+    {
+      id: useId(),
+      url: "../../../src/assets/img/3.webp",
+      name: "Katarína Habláková",
+      profession: "Prevádzková riaditeľka",
+      email: "katarina.hablakova@mostdomov.sk",
+    },
+  ];
+
+  return (
+    <section className={styles.container}>
+      <ul>
+        {data.map(({ url, name, profession, email, id }) => (
+          <li key={id}>
+            <img src={url} alt={`Fotografia ${name}`} />
+            <p className={styles.name}>{name}</p>
+            <p className={styles.profession}>{profession}</p>
+            <a className={styles.email}>{email}</a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
