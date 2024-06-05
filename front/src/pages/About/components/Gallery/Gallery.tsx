72fcc5ebe95e2b541e5a650c12fb72ac3919f333
Aleksei Falchuk <sddw28@gmail.com>
2024-06-05 13:51:19 +0200

    change gallery images paths

--- front/src/pages/About/components/Gallery/Gallery.tsx
+++ front/src/pages/About/components/Gallery/Gallery.tsx
@@ -5,14 +5,14 @@ export const Gallery = () => {
   const data = [
     {
       id: useId(),
-      url: "../../../assets/img/1.webp",
+      url: "../../assets/1.webp",
       name: "Mudr. Zuzana Hajster Vozárová",
       profession: "Odborná garantka, lekárka",
       email: "hajstervozarova@mostdomov.com",
     },
     {
       id: useId(),
-      url: "../../assets/img/2.webp",
+      url: "../../../assets/1.webp",
       name: "Mgr. Simona Hornačková, dipl.s.",
       profession: "Zdravotná sestra",
       email: "simona.hornackova@mostdomov.com",
@@ -20,7 +20,7 @@ export const Gallery = () => {
 
     {
       id: useId(),
-      url: "../assets/img/2.webp",
+      url: "../assets/1.webp",
       name: "Katarína Habláková",
       profession: "Prevádzková riaditeľka",
       email: "katarina.hablakova@mostdomov.sk",
