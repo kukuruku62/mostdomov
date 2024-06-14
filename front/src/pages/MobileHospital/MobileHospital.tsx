13cbfff919a6a1a2ec05a198b94066540a2a80c4
Aleksei Falchuk <sddw28@gmail.com>
2024-06-14 07:29:44 +0200

    fix: add key to mobile amd service page

--- front/src/pages/MobileHospital/MobileHospital.tsx
+++ front/src/pages/MobileHospital/MobileHospital.tsx
@@ -7,11 +7,11 @@ export const MobileHospital = () => {
       <div className={styles.container}>
         <div className={styles.content}>
           <h2>Mobilný hospic "Most domov"</h2>
-          {mobileHospitalData.map(({ title, items, detailTitle, detailText }) => (
-            <div className={styles.item}>
+          {mobileHospitalData.map(({ title, items, detailTitle, detailText }, index) => (
+            <div key={index} className={styles.item}>
               <h3>{title}</h3>
-              {items.map((el) => (
-                <ul>
+              {items.map((el, index) => (
+                <ul key={index}>
                   <li>{el}</li>
                 </ul>
               ))}
